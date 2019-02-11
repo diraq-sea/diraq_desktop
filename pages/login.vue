4d454dbe8373b490f3e304055a898a1113d27418
lilycoco <ry.sep27@gmail.com>
2019-02-11 19:50:28 +0900

    modified

--- pages/login.vue
+++ pages/login.vue
@@ -32,7 +32,7 @@ export default {
   },
   computed: {
     notHaveEmail() {
-      return !RegExp(/^([a-zA-Z0-9])+([a-zA-Z0-9\._-])*@([a-zA-Z0-9_-])+([a-zA-Z0-9\._-]+)+$/).test(
+      return !/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/.test(
         this.email,
       )
     },
