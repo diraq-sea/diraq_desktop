3da6b585093703df2fdc9e42ad3adfc69919c791
Ueda <to24toro@gmail.com>
2019-02-24 12:04:25 +0900

    file監視準備

--- main/modules/watcher/watcher.service.js
+++ main/modules/watcher/watcher.service.js
@@ -1,33 +1,35 @@
-// const {
-//   READY,
-//   ADD,
-//   ADDDIR,
-//   UNLINK,
-//   UNLINKDIR,
-//   CHANGE,
-//   ERROR,
-// } = require('../../../common/watcherTypes')
-// const configStore = require('../../store/config.store')
+const {
+  READY,
+  ADD,
+  ADDDIR,
+  UNLINK,
+  UNLINKDIR,
+  CHANGE,
+  ERROR,
+} = require('../../../common/watcherTypes')
+const configStore = require('../../store/config.store')
 
 // module.exports = type =>
 //   ({
 //     [READY]: () => {
 //       console.log('監視開始') // eslint-disable-next-line
-//   },
+//     },
 //     [ADD]: path => {
-//       // eslint-disable-next-line
-//     console.log("修正されました-> " + path)
+//       console.log('追加ファイル-> ' + path) // eslint-disable-next-line
+//     },
+//     [ADDDIR]: path => {
+//       console.log('追加ディレクトリ-> ' + path) // eslint-disable-next-line
+//     },
+//     [UNLINK]: path => {
+//       console.log('削除されました-> ' + path) // eslint-disable-next-line
+//     },
+//     [UNLINKDIR]: path => {
+//       console.log('削除されました-> ' + path) // eslint-disable-next-line
 //     },
 //     [CHANGE]: path => {
-//       // eslint-disable-next-line
-//     console.log("修正されました-> " + path)
+//       console.log('修正されました-> ' + path) // eslint-disable-next-line
 //     },
-//     [CHANGE]: path => {
-//       // eslint-disable-next-line
-//     console.log("修正されました-> " + path)
-//     },
-//     [CHANGE]: path => {
-//       // eslint-disable-next-line
-//     console.log("修正されました-> " + path)
+//     [ERROR]: path => {
+//       console.log('エラーです-> ' + path) // eslint-disable-next-line
 //     },
 //   }[type] || (() => {}))
