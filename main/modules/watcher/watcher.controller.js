683e7cc5345d1b6394373067c2d487ec34a1bdb6
Ueda <to24toro@gmail.com>
2019-02-26 08:11:24 +0900

    tempsfile追加

--- main/modules/watcher/watcher.controller.js
+++ main/modules/watcher/watcher.controller.js
@@ -1,6 +1,8 @@
 const chokidar = require('chokidar')
 const watcherService = require('./watcher.service')
 const watcherTypes = require('../../../common/watcherTypes')
+const mkdirIfNotExists = require('../../utils/mkdirIfNotExists')
+const { TEMPS_FILE_DIR } = require('../../const')
 var watcher = chokidar.watch('main', {
   //watch対象ディレクトリorファイル
   ignored: /[\/\\]\./, //無視する対象
@@ -9,6 +11,7 @@ var watcher = chokidar.watch('main', {
 
 module.exports = {
   init() {
+    mkdirIfNotExists(TEMPS_FILE_DIR)
     Object.values(watcherTypes).forEach(type => {
       watcher.on(type, watcherService(type))
     })
