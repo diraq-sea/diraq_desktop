88aee75aa748aee63955a1813a02db02ca361082
Mitsuhide Matsuda <genbu2011@live.jp>
2019-03-24 21:20:41 +0900

    PDF対応

--- plugins/fileIcon.js
+++ plugins/fileIcon.js
@@ -2,10 +2,12 @@ import Vue from 'vue'
 import WordIcon from '~/assets/imgs/word.png'
 import ExcelIcon from '~/assets/imgs/excel.png'
 import PowerpointIcon from '~/assets/imgs/powerpoint.png'
+import PdfIcon from '~/assets/imgs/pdf.png'
 
 Vue.prototype.$fileIcon = extname =>
   ({
     docx: WordIcon,
     xlsx: ExcelIcon,
     pptx: PowerpointIcon,
+    pdf: PdfIcon,
   }[extname])
