0e5faf9adf27dc1eb901a7e8ab7e6c2c43940fe0
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-06-17 11:26:27 +0900

    Fix MOCK_ENABLED condition

--- main/const.js
+++ main/const.js
@@ -2,6 +2,8 @@ const path = require('path')
 const HOME_DIR = require('os').homedir()
 const CONFIG_DIR = path.join(HOME_DIR, '.diraq_study')
 
+const { API_BASE_URL, MOCK_ENABLED, NODE_ENV, _NUXT_URL_ } = process.env
+
 module.exports = {
   CONFIG_DIR,
   AUTH_FILE: path.join(CONFIG_DIR, 'auth.json'),
@@ -18,11 +20,8 @@ module.exports = {
   FIRST_DROPPED_MESSAGE: name => `「${name}」をアップロードしました。`,
   TMP_FILES_DIR: path.join(CONFIG_DIR, 'tmpfiles'),
   COMMIT_FILE: path.join(CONFIG_DIR, 'commit.json'),
-  WINDOW_ORIGIN:
-    process.env.NODE_ENV === 'development'
-      ? process.env._NUXT_URL_
-      : `file://${__dirname}/index.html`,
-  API_BASE_URL: process.env.API_BASE_URL || 'http://localhost:8080/v1',
+  WINDOW_ORIGIN: NODE_ENV === 'development' ? _NUXT_URL_ : `file://${__dirname}/index.html`,
+  API_BASE_URL: API_BASE_URL || 'http://localhost:8080/v1',
   PLATFORM: { win32: 'win', darwin: 'mac', linux: 'linux' }[process.platform],
-  MOCK_ENABLED: process.env.MOCK_ENABLED || process.env.NODE_ENV === 'development',
+  MOCK_ENABLED: MOCK_ENABLED ? MOCK_ENABLED === 'true' : NODE_ENV === 'development',
 }
