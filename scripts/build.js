911f6a99213e2dadc00f9512623c8675499aa00b
hi85 <33993836+hi85gh@users.noreply.github.com>
2019-06-19 23:49:21 +0900

    Update build script

--- scripts/build.js
+++ scripts/build.js
@@ -1,13 +1,8 @@
 import webpack from 'webpack'
-import { build as electronBuilder, createTargets, Platform } from 'electron-builder'
 import mainConfig from '../webpack.config'
 
 process.env.NODE_ENV = 'production'
 
-/**
- * webpack による ECMAScript から CommonJS へのトランスコンパイルと minify を行う。
- * @returns {Promise}
- */
 function compileMain() {
   return new Promise((resolve, reject) => {
     mainConfig.mode = process.env.NODE_ENV
@@ -26,21 +21,9 @@ function compileMain() {
   })
 }
 
-/**
- * Electron アプリのビルドとパッケージを行う。
- */
 async function build() {
-  const buildTargets = []
-  const publish = process.env.PUBLISH_ENABLED || 'never'
-
-  if (process.env.BUILD_LINUX === 'true') buildTargets.push(Platform.LINUX)
-  if (process.env.BUILD_MAC === 'true') buildTargets.push(Platform.MAC)
-  if (process.env.BUILD_WINDOWS === 'true') buildTargets.push(Platform.WINDOWS)
-  if (buildTargets.length === 0) buildTargets.push(Platform.current())
-
   try {
     await compileMain()
-    await electronBuilder({ publish, targets: createTargets(buildTargets) })
   } catch (err) {
     console.error(err.stack || err) // eslint-disable-line
     process.exit(1)
