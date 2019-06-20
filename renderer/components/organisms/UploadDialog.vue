6acf18e6c5c1188f52a2c9d276de66933623eede
sfjmr <fujimori.shu.sf@gmail.com>
2019-06-20 11:27:41 +0900

    Update getEntrylistをutilsに移動した

--- renderer/components/organisms/UploadDialog.vue
+++ renderer/components/organisms/UploadDialog.vue
@@ -69,6 +69,7 @@
 
 <script>
 import FILE_EXT_TYPES from '~~/common/fileExtTypes'
+import getEntryList from '../utils/getEntryList.js'
 
 export default {
   props: {
@@ -146,40 +147,40 @@ export default {
   },
 }
 
-async function getEntryList(entrys, endPath, absoluteFliePath) {
-  let listEntry = []
+// async function getEntryList(entrys, endPath, absoluteFliePath) {
+//   let listEntry = []
 
-  for (let i = 0; i < entrys.length; i++) {
-    let entry
-    if (entrys[i][Symbol.toStringTag] === 'DataTransferItem') {
-      entry = entrys[i].webkitGetAsEntry()
-    } else {
-      entry = entrys[i]
-    }
+//   for (let i = 0; i < entrys.length; i++) {
+//     let entry
+//     if (entrys[i][Symbol.toStringTag] === 'DataTransferItem') {
+//       entry = entrys[i].webkitGetAsEntry()
+//     } else {
+//       entry = entrys[i]
+//     }
 
-    if (entry.isFile) {
-      const isHiddenFile = entry.name.slice(0, 1) === '.'
+//     if (entry.isFile) {
+//       const isHiddenFile = entry.name.slice(0, 1) === '.'
 
-      if (!isHiddenFile) {
-        listEntry.push({ name: entry.name, path: absoluteFliePath + entry.fullPath, endPath })
-      }
-    } else if (entry.isDirectory) {
-      const reader = entry.createReader()
-      const getEntries = () =>
-        new Promise(resolve => {
-          reader.readEntries(async function(entries) {
-            resolve([
-              ...listEntry,
-              ...(await getEntryList(entries, endPath + '/' + entry.name, absoluteFliePath)),
-            ])
-          })
-        })
-      listEntry = await getEntries()
-    }
-  }
+//       if (!isHiddenFile) {
+//         listEntry.push({ name: entry.name, path: absoluteFliePath + entry.fullPath, endPath })
+//       }
+//     } else if (entry.isDirectory) {
+//       const reader = entry.createReader()
+//       const getEntries = () =>
+//         new Promise(resolve => {
+//           reader.readEntries(async function(entries) {
+//             resolve([
+//               ...listEntry,
+//               ...(await getEntryList(entries, endPath + '/' + entry.name, absoluteFliePath)),
+//             ])
+//           })
+//         })
+//       listEntry = await getEntries()
+//     }
+//   }
 
-  return listEntry
-}
+//   return listEntry
+// }
 </script>
 
 <style scoped>
