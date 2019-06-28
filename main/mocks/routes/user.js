a121522ac008e8084ebee0e8ed4e95c2a78bda3a
to24toro <38037695+to24toro@users.noreply.github.com>
2019-06-28 13:54:41 +0900

    Update user.js

--- main/mocks/routes/user.js
+++ main/mocks/routes/user.js
@@ -13,9 +13,9 @@ export default {
     mockStore.add('user', create({ name, email, password })) // 本来は招待者のみユーザー登録できるので消すべき
     if (mockStore.findByKey('invite', 'email', email).email === email) {
       // mockStore.add('user', create({ name, email, password }))
+      const roomId = mockStore.findByKey('invite', 'email', email).roomId
       mockStore.deleteByKey('invite', 'email', email)
       const userId = mockStore.findByKey('user', 'email', email).id
-      const roomId = mockStore.findByKey('invite', 'email', email).roomId
       const role = ROLE_TYPES[1].id
       mockStore.add('member', create({ userId, roomId, role }))
     }
