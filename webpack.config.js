86259a1644edd51bb696be6195ef915a32a85d20
hi85 <33993836+hi85gh@users.noreply.github.com>
2020-02-22 19:03:10 +0900

    Fix webpack configuration

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,8 @@ import path from 'path'
 import webpack from 'webpack'
 import pkg from './package'
 
+const { API_BASE_URL, MOCK_ENABLED, NODE_ENV, _NUXT_URL_, S3_URL } = process.env
+
 export default {
   entry: { main: path.join(__dirname, 'main') },
   externals: [...Object.keys(pkg.dependencies || {})],
@@ -39,6 +41,11 @@ export default {
       '@@': process.cwd(),
     },
   },
-  plugins: [new webpack.NoEmitOnErrorsPlugin()],
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify({ API_BASE_URL, MOCK_ENABLED, NODE_ENV, _NUXT_URL_, S3_URL }),
+    }),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
   target: 'electron-main',
 }
